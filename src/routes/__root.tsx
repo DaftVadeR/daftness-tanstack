6d6b209a1b7e9e603f6e493bfee4d8649dc3fcca
Ross Davidson <tsuyoizuse@gmail.com>
2026-01-12 10:39:33 +0200

    Add favicon and manifest.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -36,6 +36,13 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <HeadContent />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Daftness" />
+        <link rel="manifest" href="/site.webmanifest" />
         <link
           href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap"
           rel="stylesheet"
