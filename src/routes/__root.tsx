2a231fb2f989001c3bfa9a2e76f7809260a3ee3c
Ross Davidson <tsuyoizuse@gmail.com>
2026-01-26 20:06:22 +0200

    Update all paths just to be sure.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -46,10 +46,10 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png?new=1" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg?new=1" />
         <link rel="shortcut icon" href="/favicon.ico?new=1" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?new=1" />
         <meta name="apple-mobile-web-app-title" content="Daft Dev" />
         <link rel="manifest" href="/site.webmanifest" />
 
